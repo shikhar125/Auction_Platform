c2a52020f32357cdce7b647dd2129f377cba17c2
shikhar125 <159933195+shikhar125@users.noreply.github.com>
2025-09-11 15:10:00 +0530

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,12 +4,26 @@ import ErrorHandler from "./error.js";
 import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
-  const token = req.cookies.token;
+  let token = req.cookies?.token;
+
+  // अगर cookie में token नहीं है → header से निकालो
+  if (!token && req.headers.authorization) {
+    if (req.headers.authorization.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+  }
+
   if (!token) {
     return next(new ErrorHandler("User not authenticated.", 400));
   }
+
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById(decoded.id);
+
+  if (!req.user) {
+    return next(new ErrorHandler("User not found.", 404));
+  }
+
   next();
 });
 
@@ -18,7 +32,7 @@ export const isAuthorized = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorHandler(
-          `${req.user.role} not allowed to access this resouce.`,
+          `${req.user.role} not allowed to access this resource.`,
           403
         )
       );
